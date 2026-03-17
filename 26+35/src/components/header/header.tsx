cae3bc09d64692f483607018ed4e55f7b8f9bf8c
35 <shanyom76@gmail.com>
2026-03-17 17:32:39 +0200

    Added Header and Player

---
+++ 26+35/src/components/header/header.tsx
@@ -0,0 +1,15 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import useStyles from './headerStyle';
+
+const Header = () => {
+    const {classes} = useStyles()
+
+    return (
+        <header className={classes.container}>
+            <MusicNoteIcon className={classes.icon}/>
+            <h3 className={classes.title}>SpotiPSI</h3>
+        </header>
+    )
+}
+
+export default Header
